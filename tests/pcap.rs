4be2d80fe439a4878661d634b82df1f49058bd05
Mat Wood <mat@thepacketgeek.com>
2019-09-25 13:31:42 -0700

    Add Notification Parsing
    - Parse Notification message contents (maj/minor error codes & data) into Notification struct
    - Add to Message::Notification variant
    - Add PCAP and test for notification with a message

--- tests/pcap.rs
+++ tests/pcap.rs
@@ -14,6 +14,7 @@ fn pcap1() {
     parse_pcap("res/pcap/BGP_hard_reset.cap");
     parse_pcap("res/pcap/BGP_MD5.cap");
     parse_pcap("res/pcap/BGP_MP_NLRI.cap");
+    parse_pcap("res/pcap/BGP_notification_msg.cap");
     parse_pcap("res/pcap/BGP_notification.cap");
     parse_pcap("res/pcap/BGP_redist.cap");
     parse_pcap("res/pcap/BGP_soft_reset.cap");
